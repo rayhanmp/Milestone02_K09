bdae2e0dbee7f60970017ffcbb2526c861b704a6
dhanikalisa511 <110590553+dhanikalisa511@users.noreply.github.com>
2022-08-09 14:23:25 +0700

    Maaf update lagi

--- beeastudentv1/src/App.js
+++ beeastudentv1/src/App.js
@@ -3,6 +3,12 @@ import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import AboutUs from './components/pages/Aboutus';
+import Company from './components/pages/Company';
+import FindScholarships from './components/pages/FindScholarships';
+import FindAlumnus from './components/pages/FindAlumnus';
+import Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
 
 function App() {
   return (
@@ -11,6 +17,12 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home}/>
+        <Route path='/AboutUs' exact component={AboutUs}/>
+        <Route path='/Company' exact component={Company}/>
+        <Route path='/FindScholarships' exact component={FindScholarships}/>
+        <Route path='/FindAlumnus' exact component={FindAlumnus}/>
+        <Route path='/Signup' exact component={Signup}/>
+        <Route path='/Login' exact component={Login}/>
       </Switch>
       <Footer />
     </Router>
