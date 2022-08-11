7c0c5f2762f0e41f6695c6dda5f367fba02de1fb
Pemuladigital <110378747+Pemuladigital@users.noreply.github.com>
2022-08-11 20:19:38 +0700

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./Details.css"
+
+function Details() {
+  return (
+    <>
+    <div classname="beasiswappm"> 
+
+    </div> 
+    <div>
+      <h2 className='title-desk'>DESKRIPSI BEASISWA</h2>
+      <p className='isi-deskripsi'>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        </p>
+    </div>
+    <div className='bg-bdans'></div>
+    <div>
+      <h3 className='title-bdans'>BERKAS DAN SYARAT</h3>
+      <p className='isi-bdans'>
+      "Formulir pendaftaran PPM SoM 1 softcopy fotokopi KTP/Kartu Pelajar/Identitas lainnya 1 softcopy fotokopi Kartu Keluarga 1 softcopy foto berwarna ukuran 4x6 Softcopy/scan rapor SMA/Sederajat yang telah dilegalisir dari kelas X sampai dengan semester terakhir Softcopy/scan Surat Rekomendasi dari sekolah yang ditandatangani oleh guru BK/Kepala Sekolah/Wali Kelas Kandidat dapat melampirkan informasi prestasi yang telah diraih dengan dokumen pendukung Kandidat dapat melampirkan informasi prestasi yang telah diraih dengan dokumen pendukung."
+      </p>
+    </div>
+    <div>
+      <h4 className="title-benefit">BENEFIT</h4>
+      <p className='isi-benefit'>
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit nulla ultrices et consequat leo tristique sed fringilla ut. Lacus orci, diam cras sit suspendisse tellus est. Diam mauris adipiscing ultricies ultrices lorem feugiat. Tellus felis, nibh volutpat arcu pellentesque diam, non. Magna sed viverra ipsum lorem elementum at tellus. Ipsum commodo posuere elit ac. Ligula orci nibh at nulla diam dui quisque commodo. Netus vitae consectetur tortor, vitae, fames amet auctor. Malesuada aliquet cursus nulla donec in pellentesque. Id a urna at vitae massa vitae. Tristique natoque adipiscing tincidunt eu, adipiscing sit amet turpis elementum. Tempus, sagittis turpis nulla enim habitant lectus lorem. Nullam sodales lectus tempus, ullamcorper vitae. Id volutpat lorem vestibulum odio vel arcu. Integer ultrices hendrerit sed ullamcorper. Faucibus integer urna vestibulum eget dis. Mauris leo lacus, et adipiscing sollicitudin ullamcorper a fames. Tincidunt gravida fermentum adipiscing vitae. Placerat libero vestibulum, proin lectus sit enim, viverra adipiscing vitae. Purus nisi sagittis, condimentum maecenas dictum pellentesque convallis. Gravida bibendum ullamcorper eget mauris lorem interdum et. Lacus, libero etiam facilisi dignissim. Orci, proin enim lorem aliquet leo tempor in duis sed. Rhoncus ultricies ultricies eu fringilla mi tempor, malesuada egestas adipiscing. Ac, urna, vitae nibh imperdiet magna sed non. At pretium, posuere faucibus tellus quisque in facilisi. Vestibulum dignissim sodales molestie nisl eleifend maecenas nibh. Varius proin eget suspendisse dictum leo aliquam."
+      </p>
+    </div>
+    <div classname='container-DP'>
+        <h2 classname='isi-DP'>DAFTAR BEASISWA</h2>
+    </div>
+    </>
+  )
+}
+
+export default Details
