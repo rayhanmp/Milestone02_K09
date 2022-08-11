acbe301361dcc460ffe1128093bc6215ef3daaf5
dhanikalisa511 <110590553+dhanikalisa511@users.noreply.github.com>
2022-08-12 03:33:57 +0700

    Commit sementara lagi

--- beeastudentv1/src/App.js
+++ beeastudentv1/src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/pages/Home';
+
 import Aboutus from './components/pages/Aboutus';
-import Company from './components/pages/Company';
+
 import FindScholarships from './components/pages/FindScholarships';
-import FindAlumnus from './components/pages/FindAlumnus';
+
 import Login from './components/pages/Login';
 import Signup from './components/pages/Signup';
 import Footer from './components/Footer/Footer';
-import ScrollToTop from './components/pages/ScrollToTop';
 
 function App() {
   return (
@@ -17,11 +16,10 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/' exact component={Home}/>
+
         <Route path='/Aboutus' exact component={Aboutus}/>
-        <Route path='/Company' exact component={Company}/>
+
         <Route path='/FindScholarships' exact component={FindScholarships}/>
-        <Route path='/FindAlumnus' exact component={FindAlumnus}/>
         <Route path='/Signup' exact component={Signup}/>
         <Route path='/Login' exact component={Login}/>
       </Switch>
