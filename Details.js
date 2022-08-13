f9095eee952af49a527d460f6860f758e7547836
Febryan Arota <88846095+febryanarota@users.noreply.github.com>
2022-08-13 20:49:57 +0700

    salah copas ternyata

--- Details.js
+++ Details.js
@@ -1,184 +1,109 @@
-.header-wrapper {
-    width: 100%;
-    background-image: url(../public/images/header.png);
-    background-size: cover;
-    padding-left: 100px;
-    padding-top: 70px;
-    padding-bottom: 50px;
-    color: white;
+import React from 'react';
+import "./Detail.css";
+
+function Detail() {
+  return (
+    <div className="detail-wrapper">
+      <div className="header-wrapper">
+        <div className="detail-logo-wrapper">
+          <img src="/images/PPMlogo.png" className='detail-logo' alt="detail logo"/>
+        </div>
+        <h1>Beasiswa PPM School of Management</h1>
+        <ul className='detail-info'>
+          <li><img src="/images/icon1.png" className="icon-1" alt="" />Beasiswa Pemerintah</li>
+          <li><img src="/images/icon2.png" className="icon-2" alt="" />Pendaftaran 3 Januari-8 Maret</li>
+          <li><img src="/images/icon3.png" className="icon-3" alt="" />SMA/SEDERAJAT</li>
+        </ul>
+      </div>
+      <div className="deskripsi-wrapper">
+        <div className='deskripsi-content'>
+          <h1>DESKRIPSI BEASISWA</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui enim sapien eu, convallis. Lorem in elit ac sit. Etiam metus ornare in at etiam facilisi massa fringilla. Amet facilisis aenean morbi diam commodo lobortis quisque in. Eros, enim elit, turpis arcu euismod consequat diam. Mi nibh fringilla pellentesque elementum augue scelerisque scelerisque. Ornare laoreet bibendum lacus diam. Eget malesuada bibendum interdum id risus sit ut. Scelerisque commodo aliquam felis venenatis, urna.</p>
+          <br/>
+          <p>Ac sagittis at molestie vitae nulla. Pretium risus euismod ut consequat integer aliquam viverra neque arcu. Est id pretium sed lectus dolor, in. Feugiat ipsum lacinia risus egestas ac magna hac. Vulputate turpis eleifend feugiat pellentesque arcu ac. Adipiscing viverra suscipit risus ut. Egestas in tellus, urna eu suspendisse nisl id diam.</p>
+        </div>
+      </div>
+      <div className="syarat-wrapper">
+        <div className='syarat-content'>
+          <h1>BERKAS DAN SYARAT</h1>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Dui enim sapien eu, convallis. Lorem in elit ac sit. Etiam metus ornare in at etiam facilisi massa fringilla. Amet facilisis aenean morbi diam commodo lobortis quisque in.</li>
+            <li>Eros, enim elit, turpis arcu euismod consequat diam.</li>
+            <li>Eget malesuada bibendum interdum id risus sit ut. Scelerisque commodo aliquam felis </li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+            <li>Dui enim sapien eu, convallis. Lorem in elit ac sit. Etiam metus ornare in at etiam facilisi massa fringilla. Amet facilisis aenean morbi diam commodo lobortis quisque in.</li>
+            <li>Eros, enim elit, turpis arcu euismod consequat diam.</li>
+            <li>Eget malesuada bibendum interdum id risus sit ut. Scelerisque commodo aliquam felis </li>
+          
+          </ul>
+        </div>
+      </div>
+      <div className='benefit-wrapper'>
+        <div className='benefit-content'>
+          <h1>BENEFIT</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Velit nulla ultrices et consequat leo tristique sed fringilla ut. Lacus orci, diam cras sit suspendisse tellus est. Diam mauris adipiscing ultricies ultrices lorem feugiat. Tellus felis, nibh volutpat arcu pellentesque diam, non. Magna sed viverra ipsum lorem elementum at tellus. Ipsum commodo posuere elit ac. Ligula orci nibh at nulla diam dui quisque commodo. Netus vitae consectetur tortor, vitae, fames amet auctor.</p>
+          <br/>
+          <p>Malesuada aliquet cursus nulla donec in pellentesque. Id a urna at vitae massa vitae. Tristique natoque adipiscing tincidunt eu, adipiscing sit amet turpis elementum. Tempus, sagittis turpis nulla enim habitant lectus lorem. Nullam sodales lectus tempus, ullamcorper vitae. Id volutpat lorem vestibulum odio vel arcu. Integer ultrices hendrerit sed ullamcorper. Faucibus integer urna vestibulum eget dis. Mauris leo lacus, et adipiscing sollicitudin ullamcorper a fames. Tincidunt gravida fermentum adipiscing vitae. Placerat libero vestibulum, proin lectus sit enim, viverra adipiscing vitae. Purus nisi sagittis, condimentum maecenas dictum pellentesque convallis.</p>
+          <br/>
+          <p>Gravida bibendum ullamcorper eget mauris lorem interdum et. Lacus, libero etiam facilisi dignissim. Orci, proin enim lorem aliquet leo tempor in duis sed. Rhoncus ultricies ultricies eu fringilla mi tempor, malesuada egestas adipiscing. Ac, urna, vitae nibh imperdiet magna sed non. At pretium, posuere faucibus tellus quisque in facilisi. Vestibulum dignissim sodales molestie nisl eleifend maecenas nibh. Varius proin eget suspendisse dictum leo aliquam. </p>
+        </div>
+      </div>
+      
+      <div className="daftar-wrapper">
+        <button className='daftar-button'>DAFTAR BEASISWA</button>
+      </div>
+
+      <div className="testimoni-wrapper">
+        <div className='testimoni-container'>
+          <h1>TESTIMONI</h1>
+          <div className='testi-header'>
+            <div className='testi-header1'>
+              <div className='testi-header-content'>
+                <img src="/images/ken.png" alt="" className='profile'/>
+                <div className='profile-info'>
+                  <h3 className='name'>Ken Azizan</h3>
+                  <p>Penerima Beasiswa PPM School<br /> of Management</p>
+                </div>
+              </div>
+            </div>
+            <div className='testi-header2'>
+              <div className='testi-header-content'>
+                <img src="/images/ken.png" alt="" className='profile'/>
+                <div className='profile-info'>
+                  <h3 className='name'>Ken Azizan</h3>
+                  <p>Penerima Beasiswa PPM School<br /> of Management</p>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+          
+          <div className='testimoni-content'>
+            <div className='testimoni-item1'>
+              <div className='testimoni-item-content'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Congue sodales vitae, tristique proin volutpat, auctor pharetra accumsan, tincidunt. Netus vel vulputate eu eget iaculis cursus ultricies mauris. Facilisi pharetra accumsan vulputate nibh mattis sit eget molestie. Ac, vitae sit fermentum in auctor habitant. Cursus id nec tristique commodo, vulputate elit nulla risus. At semper arcu nulla placerat dolor est eget nunc magnis.</p>
+              </div>
+            </div>
+            <div className='testimoni-item2'>
+              <div className='testimoni-item-content'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Congue sodales vitae, tristique proin volutpat, auctor pharetra accumsan, tincidunt. Netus vel vulputate eu eget iaculis cursus ultricies mauris. Facilisi pharetra accumsan vulputate nibh mattis sit eget molestie. Ac, vitae sit fermentum in auctor habitant. Cursus id nec tristique commodo, vulputate elit nulla risus. At semper arcu nulla placerat dolor est eget nunc magnis.</p>
+              </div>
+            </div>
+            
+            
+          </div>
+        </div>
+      </div>
+
+      <div className="decoration">
+        <img src="/images/decor1.png" alt="" className='decor1'/>
+        <img src="/images/decor2.png" alt="" className='decor2'/>
+        <img src="/images/decor1.png" alt="" className='decor3'/>
+        <img src="/images/decor2.png" alt="" className='decor4'/>
+      </div>
+    </div>
+  )
 }
 
-.detail-logo-wrapper {
-    width: 125px;
-    height: 125px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(247, 247, 247, 1);
-    border-radius: 20px;
-    padding-bottom: 10px;
-    margin-bottom: 18px;
-}
-
-.detail-wrapper > h1 {
-    margin-top: 18px;
-    margin-bottom: 18px;
-}
-
-.header-wrapper > ul {
-    list-style: none;
-}
-
-[class^="icon"] {
-    width: 25x;
-    height: 25px;
-    position: relative;
-    top: 8px;
-    margin-right: 10px;
-    fill: #fff;
-}
-
-.deskripsi-wrapper, .benefit-wrapper {
-    height: 100%;
-    padding-top: 50px;
-    padding-bottom: 100px;
-}
-
-.deskripsi-content, .benefit-content {
-    justify-content: center;
-    align-items: center;
-    margin: auto;
-    width : 600px;
-}
-
-.deskripsi-content > h1, .benefit-content > h1, .testimoni-container > h1 {
-    text-align: center;
-    font-weight: bold;
-    margin-bottom: 40px;
-    background: linear-gradient(90deg, #004DA9 -12.63%, #2A7CCE 125.39%);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    background-clip: text;
-    text-fill-color: transparent;
-}
-
-.syarat-wrapper {
-    padding-top : 50px;
-    padding-bottom: 50px;
-    background: linear-gradient(180deg, #2A7CCE 0%, #004DA9 100%);
-    color: white;
-}
-
-.syarat-content > h1 {
-    text-align: center;
-    margin-bottom: 40px;
-}
-
-.syarat-content {
-    width : 600px;
-    margin: auto;
-}
-
-.syarat-content > ul > li {
-    padding-bottom: 10px;
-}
-
-.daftar-wrapper {
-    text-align: center;
-    padding-bottom: 200px;
-}
-.daftar-button {
-    border-style: none;
-    background: linear-gradient(180deg, #F9841A 0%, #FEC619 122.27%);
-    box-shadow: 0px 4px 11px #F9841A;
-    border-radius: 59.5px;
-    height: 90px;
-    width: 300px;
-    font-size: 26px;
-    color: white;
-    font-weight: bold;
-    cursor: pointer;
-}
-
-.testimoni-container {
-    padding-bottom: 300px;
-}
-
-.testimoni-container > h1 {
-    margin-bottom: 100px
-}
-
-.testimoni-content, .testi-header {
-    display: flex;
-    justify-content: center;
-}
-
-.testimoni-item1, .testimoni-item2 {
-    margin: 0px 60px;
-    width: 473px;
-    height: 365px;
-    background: #F7F7F7;
-    box-shadow: 0px 7px 11px rgba(0, 0, 0, 0.28);
-    border-radius: 0px 0px 61px 61px;
-}
-
-.testi-header1, .testi-header2 {
-    margin: 0px 60px;
-    width: 473px;
-    height: 176px;
-    background: linear-gradient(0deg, #F9841A 10.45%, #FEC619 139.09%);
-    border-radius: 61px 61px 0px 0px;
-    box-shadow: 0px 7px 11px rgba(0, 0, 0, 0.28);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.testi-header-content {
-    padding-right: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.profile-info {
-    margin-left: 20px;
-    color:rgba(247, 247, 247, 1)
-}
-
-.name {
-    margin-bottom: 10px;
-}
-
-.testimoni-item-content {
-    padding: 20px 50px;
-}
-
-.decor1 {
-    position: absolute;
-    width: 140px;
-    top: 600px;
-}
-
-.decor2 {
-    position: absolute;
-    width: 140px;
-    right:0%;
-    top: 470px;
-}
-
-.decor3 {
-    position: absolute;
-    width: 140px;
-    top: 1650px;
-}
-
-.decor4 {
-    position: absolute;
-    width: 140px;
-    right:0%;
-    top: 1350px;
-}
-
-
-
+export default Detail
