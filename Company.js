4b40742a589d48477ca8cffa24280a30b31a3ba3
Kenazizan01 <110590557+Kenazizan01@users.noreply.github.com>
2022-08-14 12:17:42 +0700

    add Company

---
+++ Company.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import "./Company.css"
+
+function Company() {
+  return (
+    <>
+      <div className="company-wrapper">
+        <div className="company-title">
+          <h1>Company/Institute</h1>
+        </div>
+        <div className='company-body'>
+          <div className="searchInput-company">
+            <div className='searchbar'>
+              <input id="searchInput" type="text" className="search-company"placeholder="Search here..." />
+            </div>
+            <div className='search-button'>
+              <button className="company-button"><img src="/images/search.png" alt=""/></button>
+            </div>
+          </div>
+          <div className='company-cards-wrapper'>
+            <div className='company-card'>
+              <div className='card1'> 
+                <div className='card-header'></div>
+                <div className='title-wrapper'>
+                  <img src="https://digitalfinger.id/wp-content/uploads/2019/12/logo-ppm-school-new-768x257.png" className="card-logo"alt=""/>
+                  <div className='card-title'>PPM School of Management</div>
+                </div>
+                <div className='card-body'>
+                PPM School of Management merupakan salah satu institusi pendidikan tinggi yang unggul di bidang studi Manajemen Bisnis dan Akuntansi di Indonesia. 
+                </div>
+                <div className='card-link'>
+                  <a href='/' className='detail-cardlink'>Lihat selengkapnya</a>
+                </div>
+              </div>
+              
+            </div>
+            <div className='company-card'>
+              <div className='card1'> 
+                <div className='card-header'></div>
+                <div className='title-wrapper'>
+                  <img src="https://digitalfinger.id/wp-content/uploads/2019/12/logo-ppm-school-new-768x257.png" className="card-logo"alt=""/>
+                  <div className='card-title'>PPM School of Management</div>
+                </div>
+                <div className='card-body'>
+                PPM School of Management merupakan salah satu institusi pendidikan tinggi yang unggul di bidang studi Manajemen Bisnis dan Akuntansi di Indonesia. 
+                </div>
+                <div className='card-link'>
+                  <a href='/' className='detail-cardlink'>Lihat selengkapnya</a>
+                </div>
+              </div>
+              
+            </div>
+            <div className='company-card'>
+              <div className='card1'> 
+                <div className='card-header'></div>
+                <div className='title-wrapper'>
+                  <img src="https://digitalfinger.id/wp-content/uploads/2019/12/logo-ppm-school-new-768x257.png" className="card-logo"alt=""/>
+                  <div className='card-title'>PPM School of Management</div>
+                </div>
+                <div className='card-body'>
+                PPM School of Management merupakan salah satu institusi pendidikan tinggi yang unggul di bidang studi Manajemen Bisnis dan Akuntansi di Indonesia. 
+                </div>
+                <div className='card-link'>
+                  <a href='/' className='detail-cardlink'>Lihat selengkapnya</a>
+                </div>
+              </div>
+              
+            </div>
+            <div className='company-card'>
+              <div className='card1'> 
+                <div className='card-header'></div>
+                <div className='title-wrapper'>
+                  <img src="https://digitalfinger.id/wp-content/uploads/2019/12/logo-ppm-school-new-768x257.png" className="card-logo"alt=""/>
+                  <div className='card-title'>PPM School of Management</div>
+                </div>
+                <div className='card-body'>
+                PPM School of Management merupakan salah satu institusi pendidikan tinggi yang unggul di bidang studi Manajemen Bisnis dan Akuntansi di Indonesia. 
+                </div>
+                <div className='card-link'>
+                  <a href='/' className='detail-cardlink'>Lihat selengkapnya</a>
+                </div>
+              </div>
+              
+            </div>
+            <div className='company-card'>
+              <div className='card1'> 
+                <div className='card-header'></div>
+                <div className='title-wrapper'>
+                  <img src="https://digitalfinger.id/wp-content/uploads/2019/12/logo-ppm-school-new-768x257.png" className="card-logo"alt=""/>
+                  <div className='card-title'>PPM School of Management</div>
+                </div>
+                <div className='card-body'>
+                PPM School of Management merupakan salah satu institusi pendidikan tinggi yang unggul di bidang studi Manajemen Bisnis dan Akuntansi di Indonesia. 
+                </div>
+                <div className='card-link'>
+                  <a href='/' className='detail-cardlink'>Lihat selengkapnya</a>
+                </div>
+              </div>
+              
+            </div>
+            <div className='company-card'>
+              <div className='card1'> 
+                <div className='card-header'></div>
+                <div className='title-wrapper'>
+                  <img src="https://digitalfinger.id/wp-content/uploads/2019/12/logo-ppm-school-new-768x257.png" className="card-logo"alt=""/>
+                  <div className='card-title'>PPM School of Management</div>
+                </div>
+                <div className='card-body'>
+                PPM School of Management merupakan salah satu institusi pendidikan tinggi yang unggul di bidang studi Manajemen Bisnis dan Akuntansi di Indonesia. 
+                </div>
+                <div className='card-link'>
+                  <a href='/' className='detail-cardlink'>Lihat selengkapnya</a>
+                </div>
+              </div>
+              
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Company
