49f076dd91d2ad90d7a43c1b5d9d6606a75f81c1
Dhanika Novlisariyanti <110590843+dhanikanovlisa@users.noreply.github.com>
2022-08-15 22:42:00 +0700

    Add files via upload

---
+++ AlumnusData.js
@@ -0,0 +1,59 @@
+
+
+
+const dataAlumnus = [
+    {
+        id: '1',
+        namaAlunus : 'Ken Azizan',
+    },
+    {
+        id: '2',
+        namaAlunus : 'Hanif Muhammad Zhafran'
+    },
+    {
+        id: '3',
+        namaAlunus : 'I Dewa Made Manu Pradnyana '
+    },
+    {
+        id: '4',
+        namaAlunus : 'Kenneth Dave Bahana'
+    },
+    {
+        id: '5',
+        namaAlunus : 'Dhanika Novlisariyanti'
+    },
+    {
+        id: '6',
+        namaAlunus : 'Febryan Arota Hia '
+    },
+    {
+        id: '7',
+        namaAlunus : 'Nadine Aliya Putri'
+    },
+    {
+        id: '8',
+        namaAlunus : 'Muhammad Rifko Favian'
+    },
+    {
+        id: '9',
+        namaAlunus : 'Rayhan Maheswara Pramanda '
+    },
+    {
+        id: '10',
+        namaAlunus : 'Go Dillon Audris'
+    },
+    {
+        id: '11',
+        namaAlunus : 'Muhammad A. Bismaramadhan '
+    },
+    {
+        id: '12',
+        namaAlunus : 'Sri Laksmi Purwoningtyas '
+    },
+    {
+        id: '13',
+        namaAlunus : 'Nurshafa Qanita'
+    },
+]
+
+export default dataAlumnus;
